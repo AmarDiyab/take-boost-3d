f51538a8232e89a605e68bf89370d21745c70081
Amar Diyab <109582572+AmarDiyab@users.noreply.github.com>
2024-04-27 12:32:55 +0530

    Added about section content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import arrow from "../assets/logo.svg";
 
 const About = () => {
   return (
-    <div className="h-screen">
+    <div className="h-screen flex justify-between">
       {/* <img src={arrow} alt="arrow" /> */}
       <div className="border border-white rounded-full h-[80vh] w-[80vh] flex justify-center items-center text-8xl font-semibold -ml-10" id="#about">
         <p className="flex flex-col items-end ml-10">
@@ -11,6 +11,16 @@ const About = () => {
           <span className="3">🤧 NOT</span>
           <span className="flex-auto self-start">SICK</span>
         </p>
+      </div>
+
+      <div className="flex flex-col justify-center max-w-[20%] mr-[10rem] gap-3">
+        <p className="text-xs">BOOST HELPS YOU GET SICK LESS</p>
+        <h4 className="text-4xl font-semibold">BE PROACTIVE NOT REACTIVE ABOUT YOUR IMMUNITY</h4>
+        <h6 className="text-xl">No one gives a f*ck about their immune system unless they have to... and it took us a pandemic to realize that. BOOST is here to fix that.</h6>
+
+        <button className="w-[80%] border-2 border-white rounded-full py-2 mt-3">
+            BUY BOOST
+        </button>
       </div>
     </div>
   );
