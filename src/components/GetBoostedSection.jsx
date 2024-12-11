6b6284f009ea7ffbcdbd1ebf27bb64ce76295b81
Amar Diyab <109582572+AmarDiyab@users.noreply.github.com>
2024-12-11 13:03:54 +0530

    Added Arrow Icon in get boosted section

--- src/components/GetBoostedSection.jsx
+++ src/components/GetBoostedSection.jsx
@@ -1,10 +1,19 @@
+import { PiArrowUpRightLight, PiArrowUpRightThin } from "react-icons/pi";
 
 const GetBoostedSection = () => {
   return (
-    <div className='py-20 mx-20 max-lg:mx-12'>
-        <p className='text-[10rem] max-lg:text-6xl leading-[10rem] font-extrabold'>GET <br />BOOSTED</p>
+    <div className="py-20 mx-20 max-lg:mx-12">
+      <p className="text-[10rem] max-lg:text-6xl leading-[10rem] font-extrabold">
+        GET <br />
+        <div className="flex gap-3 items-center">
+        <span>BOOSTED</span>
+        <div className="border border-black h-[120px] w-[120px] rounded-full flex justify-center items-center">
+          <PiArrowUpRightThin className="hover:rotate-[360deg] ease-soft-spring duration-300" />
+        </div>
+        </div>
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default GetBoostedSection
+export default GetBoostedSection;
