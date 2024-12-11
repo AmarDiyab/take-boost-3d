b0041f56b83ccf75e03ec2577b761bd97aa0f1b8
Amar Diyab <109582572+AmarDiyab@users.noreply.github.com>
2024-12-11 12:43:10 +0530

    Added custom cursor

---
+++ src/CustomCursor.jsx
@@ -0,0 +1,53 @@
+import { useRef, useEffect } from "react";
+import gsap from "gsap";
+
+export default function CursorFollower() {
+  // Using useRef() to store the cursor element reference
+  const cursorRef = useRef(null);
+  // Check if it's a touch device
+  const isTouchDevice = "ontouchstart" in window;
+  // useEffect() to only execute this code
+  // when the HTML is ready as well as the element reference
+  useEffect(() => {
+    const cursor = cursorRef.current;
+    // If device is touchable or cursor element
+    // doesn't exist, stop here
+    if (isTouchDevice || !cursor) {
+      return;
+    }
+    // Using mousemove() to animate the element cursor
+    // based on the mouse cursor position (x and y)
+    window.addEventListener("mousemove", (e) => {
+      const { target, x, y } = e;
+      // check if the mouse cursor is over some link or button
+      const isTargetLinkOrBtn =
+        target?.closest("a") || target?.closest("button");
+      // using the GSAP power to animate some properties
+      gsap.to(cursor, {
+        x: x,
+        y: y,
+        duration: 0.7,
+        // ease: "power4",
+        opacity: isTargetLinkOrBtn ? 1 : 1,
+        background: isTargetLinkOrBtn ? "purple" : "",
+        borderColor: isTargetLinkOrBtn ? "purple" : "",
+        transform: `scale(${isTargetLinkOrBtn ? 0.7 : 1})`,
+      });
+    });
+    // Using mouseleave() to animate the element cursor
+    // when the mouse cursor is moved off the page
+    document.addEventListener("mouseleave", () => {
+      gsap.to(cursor, {
+        duration: 0.7,
+        opacity: 1,
+      });
+    });
+  }, []);
+
+  return (
+    <div
+      className="fixed top-0 left-0 h-8 w-8 border rounded-full border-black z-[1000] pointer-events-none select-none"
+      ref={cursorRef}
+    ></div>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import SmoothScrolling from "./utils/lenis";
+import CursorFollower from "./CustomCursor.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <CursorFollower />
     <SmoothScrolling>
       <App />
     </SmoothScrolling>
