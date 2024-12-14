b80f8e4db706b84f6d06d52817819ec32084d7a0
Amar Diyab <109582572+AmarDiyab@users.noreply.github.com>
2024-12-14 16:06:51 +0530

    added link to get boosted arrow

--- src/components/GetBoostedSection.jsx
+++ src/components/GetBoostedSection.jsx
@@ -1,5 +1,5 @@
 import { PiArrowUpRightThin } from "react-icons/pi";
-import { useLocation } from "react-router";
+import { Link, useLocation } from "react-router";
 
 const GetBoostedSection = () => {
   const location = useLocation();
@@ -9,10 +9,16 @@ const GetBoostedSection = () => {
       <p className="text-[10rem] max-lg:text-6xl leading-[10rem] font-extrabold">
         GET <br />
         <div className="flex gap-3 items-center">
-        <span>BOOSTED</span>
-        <div className={`border-2  h-[120px] w-[120px] rounded-full flex justify-center items-center ${location.pathname === "/about" ? "border-black" : "border-white"}`}>
-          <PiArrowUpRightThin className="hover:rotate-[360deg] ease-soft-spring duration-300" />
-        </div>
+          <span>BOOSTED</span>
+          <Link to="/shop">
+            <div
+              className={`border-2  h-[120px] w-[120px] rounded-full flex justify-center items-center ${
+                location.pathname === "/about" ? "border-black" : "border-white"
+              }`}
+            >
+              <PiArrowUpRightThin className="hover:rotate-[360deg] ease-soft-spring duration-300" />
+            </div>
+          </Link>
         </div>
       </p>
     </div>
